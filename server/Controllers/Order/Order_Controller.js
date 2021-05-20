253ed34313e74f51b787971b1e51d1e9ad3a330c
Prasadraj6897 <prasadraj32895@gmail.com>
2021-05-20 09:32:24 +0530

    Added remove cart item API after placing order

--- server/Controllers/Order/Order_Controller.js
+++ server/Controllers/Order/Order_Controller.js
@@ -1,19 +1,28 @@
 import Order from '../../Models/Orders.js'
-
+import Cart from '../../Models/Cart.js'
 
 export const addOrder = (req, res) => {
-        req.body.user = req.user.id
-        const order = new Order(req.body)
-        order.save((error, order)=>{
-            if(error)
-            {
-                return res.status(400).json({error})
-            }
-            if(order)
-            {
-                return res.status(200).json({order})
-            }
-        })
+    Cart.deleteOne({user:req.user.id}).exec((error,result)=>{
+        if(error)
+        {
+            return res.status(400).json({error})
+        }
+        if(result){
+            req.body.user = req.user.id
+            const order = new Order(req.body)
+            order.save((error, order)=>{
+                if(error)
+                {
+                    return res.status(400).json({error})
+                }
+                if(order)
+                {
+                    return res.status(200).json({order})
+                }
+            })
+        }
+    })
+        
 
 }
 
